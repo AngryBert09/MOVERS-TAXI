3ceaeb91bf04cec8e45707faef7469c313b6d19b
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2025-03-18 10:07:13 +0800

    Update job-view.blade.php

--- resources/views/jobs/job-view.blade.php
+++ resources/views/jobs/job-view.blade.php
@@ -11,11 +11,11 @@
         <div class="header">
 
             <!-- Logo -->
-            <div class="header-left">
+            {{-- <div class="header-left">
                 <a href="index.html" class="logo">
                     <img src="assets/img/logo.png" width="40" height="40" alt="">
                 </a>
-            </div>
+            </div> --}}
             <!-- /Logo -->
 
             <!-- Header Title -->
