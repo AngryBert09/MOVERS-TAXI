bc92f854b42541a265c97e734b9dd54e023aee70
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2025-04-24 08:24:05 +0800

    Update job-applicants.blade.php

--- resources/views/jobs/job-applicants.blade.php
+++ resources/views/jobs/job-applicants.blade.php
@@ -163,7 +163,7 @@
 
                                             <td>
                                                 <button type="button" class="btn btn-sm btn-primary viewFileBtn"
-                                                    data-file="{{ asset('storage/app/public' . $applicant->resume) }}"
+                                                    data-file="{{ asset('storage/app/public/' . $applicant->resume) }}"
                                                     data-toggle="modal" data-target="#fileViewModal">
                                                     <i class="fa fa-eye"></i> View
                                                 </button>
