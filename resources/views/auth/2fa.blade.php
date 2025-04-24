8509dbc05e03c31ae6ab60e6eeed350f5068c5c1
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2025-04-24 08:13:24 +0800

    ATTEMPT FIX : IMAGES

--- resources/views/auth/2fa.blade.php
+++ resources/views/auth/2fa.blade.php
@@ -87,14 +87,14 @@
     <!-- /Main Wrapper -->
 
     <!-- jQuery -->
-    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
 
     <!-- Bootstrap Core JS -->
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
     <!-- Custom JS -->
-    <script src="assets/js/app.js"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
 
 </body>
 
