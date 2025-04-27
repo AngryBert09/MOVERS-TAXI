697a6769a2a549edeeda6eddbfe1d5b0798141e5
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2025-04-27 10:43:54 +0800

    Update TrainingController.php

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Carbon;
 use App\Models\TrainingAchievement;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Storage;
 
 class TrainingController extends Controller
